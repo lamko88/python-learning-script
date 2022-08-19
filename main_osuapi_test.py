b64a116a896948c9c1aa5fe580ddf68248051ce2
Lam <lamko@oregonstate.edu>
2022-08-19 13:57:39 -0700

    Fixed strings quote

--- main_osuapi_test.py
+++ main_osuapi_test.py
@@ -3,9 +3,7 @@ import osu_apis
 
 
 def main():
-
-    """Creating an object to an OSU API class"""
-
+    # Creating an object to an OSU API class
     api_class_obj = osu_apis.osu_api_class()
 
     """Get user's config data
@@ -17,16 +15,14 @@ def main():
     """
     config_dict = api_class_obj.read_config()
 
-    """Get access_token using user's config data
-    """
+    # Get access_token using user's config.json data
     access_token = api_class_obj.get_access_token(
-                                      config_dict['client_id'],
-                                      config_dict['client_secret'],
-                                      config_dict['grant_type'],
-                                      config_dict['host_url']
+                        config_dict['client_id'],
+                        config_dict['client_secret'],
+                        config_dict['host_url']
                     )
 
-    """Get header data using access_token value"""
+    # Get header data using access_token value
     headers = api_class_obj.get_headers(access_token)
 
     """User's query data inputs
@@ -40,19 +36,19 @@ def main():
     """
     while True:
         try:
-            onid = str(input("\nEnter ONID name: "))
-            building_abbr = str(input("Enter Building Abbr(Ex: JSB): "))
-            calendar_year = str(input("Enter Calendar Year(Example: 2022): "))
+            onid = str(input('\nEnter ONID name: '))
+            building_abbr = str(input('Enter Building Abbr(Ex: JSB): '))
+            calendar_year = str(input('Enter Calendar Year(Example: 2022): '))
         except ValueError:
-            print("\nInvalid data type or Missing data. " +
-                  "Please try again...\n")
+            print('\nInvalid data type or Missing data. ' +
+                  'Please try again...\n')
             continue
         if not (
                 onid.isalpha() and building_abbr.isalpha() and
                 calendar_year.isdigit()
                ):
-            print("\nOops, invalid data type. " +
-                  "Please try entering the data again...\n")
+            print('\nOops, invalid data type. ' +
+                  'Please try entering the data again...\n')
         else:
             break
 
@@ -62,53 +58,39 @@ def main():
           'calendarYear': calendar_year,
     }
 
+    # Get directory response data using user's data inputs
     print(
-            "\nResponse Data for Directory:\n",
-            api_class_obj.get_response(
-                config_dict['host_url'],
-                config_dict['directory_url'],
-                headers=headers,
-                params=params)
+        '\nResponse Data for Directory:\n',
+        api_class_obj.get_response(
+            config_dict['host_url'],
+            config_dict['directory_url'],
+            headers=headers,
+            params=params
+        )
     )
 
-    """Get response Directory query data using these input parameters
-
-    :param host_url: user's defined host_url
-    :type: string
-    :param directory_url: user's query choice: directory, location, or term
-    :type: string
-    :param headers: header information to get query response
-    :type: json
-    :param params: user's input parameters
-    :type: string
-    :return: query data
-    :rtype: json
-    """
-
-    """Get location response data using user's data inputs"""
-
+    # Get location response data using user's data inputs
     print(
-            "\nResponse Data for Location:\n",
-            api_class_obj.get_response(
-                              config_dict['host_url'],
-                              config_dict['location_url'],
-                              headers=headers,
-                              params=params)
+        '\nResponse Data for Location:\n',
+        api_class_obj.get_response(
+            config_dict['host_url'],
+            config_dict['location_url'],
+            headers=headers,
+            params=params
+        )
     )
 
-    """Get terms response data using user's data inputs
-    """
+    # Get terms response data using user's data inputs
     print(
-            "\nResponse Data for Terms:\n",
-            api_class_obj.get_response(
-                              config_dict['host_url'],
-                              config_dict['terms_url'],
-                              headers=headers,
-                              params=params
-            )
+        '\nResponse Data for Terms:\n',
+        api_class_obj.get_response(
+            config_dict['host_url'],
+            config_dict['terms_url'],
+            headers=headers,
+            params=params
+        )
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-    
--- osu_apis.py
+++ osu_apis.py
@@ -3,13 +3,15 @@ from os.path import exists
 
 import json
 
+import string
+
 import requests
 
 
 class osu_api_class:
 
     def read_config(self):
-        """Read in user's config.json file if exist"""
+        # Read in user's config.json file if exist
         if exists('config.json'):
             with open('config.json', 'r') as cfg:
                 data = json.load(cfg)
@@ -20,90 +22,66 @@ class osu_api_class:
             :param v: value in dictionary data
             """
             for k, v in data.items():
-                if (k == 'client_id' and v.isalnum() and v.strip()):
+                if (k == 'client_id' and v):
                     continue
-                elif (k == 'client_secret' and v.isalnum() and v.strip()):
+                elif (k == 'client_secret' and v):
                     continue
-                elif (k == 'grant_type' and v.isascii() and v.strip()):
+                elif (k == 'host' and v):
                     continue
-                elif (k == 'host' and v.isascii() and v.strip()):
+                elif (k == 'directory' and v):
                     continue
-                elif (k == 'directory' and v.isascii() and v.strip()):
+                elif (k == 'locations' and v):
                     continue
-                elif (k == 'locations' and v.isascii() and v.strip()):
-                    continue
-                elif (k == 'terms' and v.isascii() and v.strip()):
+                elif (k == 'terms' and v):
                     continue
                 else:
-                    print("\nInvalid data", (k, v), "in config.json file.")
-                    print("Exiting!\n")
+                    print(
+                        f'\nInvalid data {k, v} in config.json file. Exiting!!'
+                        )
                     exit()
 
-            """Creating a dictionary using the config.file data
-
-            :param client_id: User's client_id value
-            :param client_secret: User's client_secret
-            :param grant_type: This a hard-coded as "client_credentials"
-            :param host_url: Host url
-            :param directory_url: Directory url to be used in query
-            :param location_url: Location url to be used in query
-            :param terms_url: Terms url to be used in query
-            :returns: Dictionary, config_dict
-            """
             config_dict = {
-                    'client_id': data['client_id'],
-                    'client_secret': data['client_secret'],
-                    'grant_type': "client_credentials",
-                    'host_url': data['host'],
-                    'directory_url': data['directory'],
-                    'location_url': data['locations'],
-                    'terms_url': data['terms'],
+                'client_id': data['client_id'],
+                'client_secret': data['client_secret'],
+                'host_url': data['host'],
+                'directory_url': data['directory'],
+                'location_url': data['locations'],
+                'terms_url': data['terms'],
             }
 
             return config_dict
         else:
-            print("\nconfig.json file not found. Exiting!!!\n")
+            print('\nconfig.json file not found. Exiting!!!\n')
             exit()
 
-    def get_access_token(self, client_id, client_secret, grant_type, host):
-        """Get access token using these input parameters listed below.
+    def get_access_token(
+        self,
+        client_id: string,
+        client_secret: string,
+        host: string
+    ) -> string:
+        """ Get access token using these input parameters listed below.
 
         :param client_id: User's client_id value
-        :type client_id: string
         :param client_secret: User's client_secret
-        :type client_secret: string
-        :param grant_type: Hard-coded value, "client_credentials"
-        :type grant_type: string
+        :param grant_type: Hard-coded value, 'client_credentials'
         :param host: host url
-        :type host: string
         """
 
         params = {
-                    'client_id': client_id,
-                    'client_secret': client_secret,
-                    'grant_type': grant_type,
-                }
-
-        """Parameter values to be used as client's information to request
-           for access token
-
-        :param client_id: User's client_id value
-        :type client_id: string
-        :param client_secret: User's client_secret value
-        :type client_secret: string
-        :param grant_type: Hard-coded value, "client_credentials"
-        :type grant_type: string
-        :return: token, the access token
-        :rtype: string
-        """
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'grant_type': 'client_credentials',
+        }
 
         token_response = requests.post(f'{host}oauth2/token', params)
-        if (token_response.status_code != 200):
+
+        status_code = token_response.status_code
+
+        if (status_code != 200):
             print(
-                    "\nToken response error. " +
-                    "Expected status code is 200." +
-                    "\nPlease review client credentials in " +
-                    "config.json for valid data.\n"
+                f'\nError!! Expected 200 but got {status_code}.\
+                \nPlease review config.json file. Exiting!!!\n'
             )
             exit()
         else:
@@ -119,9 +97,9 @@ class osu_api_class:
         :rtype: string
         """
         return {
-                    'Authorization': f'Bearer {access_token}',
-                    'Content-Type': 'application/json'
-                }
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
 
     def get_response(self, host, query_directory, headers, params):
         """Get response from query directory using the following inputs:
@@ -137,21 +115,19 @@ class osu_api_class:
         """
 
         get_query_response = requests.get(
-                            f'{host}{query_directory}',
-                            headers=headers,
-                            params=params
-                        )
+                                f'{host}{query_directory}',
+                                headers=headers,
+                                params=params
+                                )
 
-        if (get_query_response.status_code != 200):
+        status_code = get_query_response.status_code
+
+        if (status_code != 200):
             print(
-                    "\nHeader response errors. " +
-                    "Expected status code = 200, but got",
-                    get_query_response.status_code, "."
-                    "\nPlease review query directories in config.json " +
-                    "for valid data.\n"
-                )
+                f'\nError!! Expected 200 but got {status_code}.\
+                \nPlease review config.json file. Exiting!!!\n'
+            )
             exit()
         else:
             data = get_query_response.json()
             return data['data']
-            
